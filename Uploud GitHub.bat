fcd3039de11219ff31c5b4d309a6148ba6291f2f
pedrocesario <pedro.cesario@outlook.com.br>
2021-11-04 09:21:28 -0300

    first commit

--- Uploud GitHub.bat
+++ Uploud GitHub.bat
@@ -1,5 +1,5 @@
 git init
 git add .
 git commit -m "first commit"
-git remote add origin master https://github.com/pedrocesario/reajuste-salarial.git
+git remote add origin master https://github.com/pedrocesario/ReajusteSalarial
 git push -u origin master
